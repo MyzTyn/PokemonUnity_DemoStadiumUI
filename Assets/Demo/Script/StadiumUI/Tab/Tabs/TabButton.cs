22855381139160ac3fec536a2d2f49cb222c9f9a
Herb M <herbertmilhomme@gmail.com>
2022-01-07 12:16:39 -0500

    Added Tab Paging System
    Used tabs to cycle between storage boxes used to store player's pokemon in PC.
    Used youtube video:
    https://www.youtube.com/watch?v=211t6r12XPQ

---
+++ Assets/Demo/Script/StadiumUI/Tab/Tabs/TabButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// </summary>
+/// <remarks>
+/// https://www.youtube.com/watch?v=211t6r12XPQ
+/// </remarks>
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+	public TabGroup tabGroup;
+	public bool disabled;
+	public Image background;
+	public Color? tabIdleOverride;
+	public Color? tabActiveOverride;
+	public Color? tabHoverOverride;
+	public Color? tabTextIdleOverride;
+	public Color? tabTextActiveOverride;
+	public Color? tabTextHoverOverride;
+	public UnityEvent onTabSelected;
+	public UnityEvent onTabDeselected;
+
+	public virtual void Select() { if (onTabSelected != null) onTabSelected.Invoke(); }
+	public virtual void Deselect() { if (onTabDeselected != null) onTabDeselected.Invoke(); }
+	public virtual void Select(UnityAction action) { if (action != null) onTabSelected.AddListener(action); }
+	public virtual void Deselect(UnityAction action) { if (action != null) onTabDeselected.AddListener(action); }
+
+	private void Start()
+	{
+		background = GetComponent<Image>();
+		tabGroup?.Subscribe(this);
+	}
+
+	public virtual void OnPointerEnter(PointerEventData eventData)
+	{
+		tabGroup?.OnTabEnter(this);
+	}
+
+	public virtual void OnPointerExit(PointerEventData eventData)
+	{
+		tabGroup?.OnTabExit(this);
+	}
+
+	public virtual void OnPointerClick(PointerEventData eventData)
+	{
+		tabGroup?.OnTabSelected(this);
+	}
+}
---
+++ Assets/Demo/Script/StadiumUI/Tab/Tabs/TabGroup.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// </summary>
+/// <remarks>
+/// https://www.youtube.com/watch?v=211t6r12XPQ
+/// </remarks>
+public class TabGroup : MonoBehaviour
+{
+	/// <summary>
+	/// Group of <see cref="GameObject"/> to cycle between when navigating <seealso cref="tabButtons"/>
+	/// </summary>
+	public List<GameObject> tabPages;
+	public List<TabButton> tabButtons;
+	public Color tabIdle;
+	public Color tabActive;
+	public Color tabHover;
+	public Color tabTextIdle;
+	public Color tabTextActive;
+	public Color tabTextHover;
+	public TabButton selectedTab;
+
+	public virtual void Subscribe(TabButton tab)
+	{
+		if (tabButtons == null)
+		{
+			tabButtons = new List<TabButton>();
+		}
+
+		tabButtons.Add(tab);
+	}
+
+	public virtual void OnTabEnter(TabButton tab)
+	{
+		if (tab.disabled) return;
+		ResetTabs();
+		if (selectedTab == null || tab != selectedTab)
+        {
+			tab.background.color = tab.tabHoverOverride ?? tabHover;
+			//tab.background.color = tab.tabTextHoverOverride ?? tabTextHover;
+        }
+		//if (tab.tabHoverOverride != null)
+		//	tab.background.color = tab.tabHoverOverride.Value;
+	}
+
+	public virtual void OnTabExit(TabButton tab)
+	{
+		if (tab.disabled) return;
+		ResetTabs();
+	}
+
+	public virtual void OnTabSelected(TabButton tab)
+	{
+		if (tab.disabled) return;
+		if (selectedTab != null)
+			selectedTab.Deselect();
+		selectedTab = tab;
+		selectedTab.Select();
+		ResetTabs();
+		tab.background.color = tab.tabActiveOverride ?? tabActive;
+		//if (tab.tabActiveOverride != null)
+		//	tab.background.color = tab.tabActiveOverride.Value;
+		int index = tab.transform.GetSiblingIndex();
+		for (int i = 0; i < tabPages.Count; i++)
+			if (i == index)
+				tabPages[i]?.SetActive(true);
+		else
+				tabPages[i]?.SetActive(false);
+	}
+
+	public virtual void ResetTabs()
+	{
+		foreach (TabButton tab in tabButtons)
+		{
+			if (selectedTab != null && selectedTab == tab) continue;
+			tab.background.color = tab.tabIdleOverride ?? tabIdle;
+			//if (tab.tabIdleOverride != null)
+			//	tab.background.color = tab.tabIdleOverride.Value;
+		}
+	}
+}
